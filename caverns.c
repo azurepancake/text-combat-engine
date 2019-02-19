a3f9fdf8469762cefb57fd64e8926e4505434482
Tristan Evans <tristan.evans@officedepot.com>
2019-02-19 13:27:45 -0500

    retab

--- caverns.c
+++ caverns.c
@@ -7,16 +7,16 @@ void westCaverns(struct Player *player)
 	// enter battle
 	if(battle(player, enemy) == 1) {
 		typeout("\nYou've defeated the beast that emerged from the cloak of darkness. You retrieve a stick of rough wood which the monster used as a weapon.\n\n");
-        printf("What do you do?\n> ");
+		printf("What do you do?\n> ");
 
 		char *input = malloc(MAX_INPUT_SZ);
-        char *word;
-        const char *keywords[2] = { "enter", "return" };
-        int length = (sizeof(keywords) / sizeof(*keywords)) - 1;
+		char *word;
+		const char *keywords[2] = { "enter", "return" };
+		int length = (sizeof(keywords) / sizeof(*keywords)) - 1;
 		getchar();
 		scanf("%*[^\n]%*c");
-        fgets(input, MAX_INPUT_SZ, stdin);
-        word = scanner(input, keywords, length);
+		fgets(input, MAX_INPUT_SZ, stdin);
+		word = scanner(input, keywords, length);
 	}
 }
 
@@ -27,11 +27,11 @@ void westCavernsEntrace(struct Player *player)
 		printf("What do you do?\n> ");
 
 		char *input = malloc(MAX_INPUT_SZ);
-	    char *word;
-	    const char *keywords[2] = { "enter", "return" };
-	    int length = (sizeof(keywords) / sizeof(*keywords)) - 1;
-	    fgets(input, MAX_INPUT_SZ, stdin);
-	    word = scanner(input, keywords, length);
+		char *word;
+		const char *keywords[2] = { "enter", "return" };
+		int length = (sizeof(keywords) / sizeof(*keywords)) - 1;
+		fgets(input, MAX_INPUT_SZ, stdin);
+		word = scanner(input, keywords, length);
 
 		if(strcmp(word, "enter") == 0) {
 			westCaverns(player);
@@ -45,26 +45,26 @@ void westCavernsEntrace(struct Player *player)
 
 void startCaverns(struct Player *player)
 {
-    printf("\nLoading caverns binary");
-    typeout("........ ");
-    printf("OK!\n\n\n");
-    printf("//// T H E  C A V E R N S ////\n\n");
+	printf("\nLoading caverns binary");
+	typeout("........ ");
+	printf("OK!\n\n\n");
+	printf("//// T H E	C A V E R N S ////\n\n");
 
-    char *input = malloc(MAX_INPUT_SZ);
-    char *word;
-    const char *keywords[4] = { "north", "east", "south", "west" };
-    int length = (sizeof(keywords) / sizeof(*keywords)) - 1;
+	char *input = malloc(MAX_INPUT_SZ);
+	char *word;
+	const char *keywords[4] = { "north", "east", "south", "west" };
+	int length = (sizeof(keywords) / sizeof(*keywords)) - 1;
 
 	printf("You now stand at the southern end of a large cavern. There is nothing, but a solid wall of stone behind you. The area is softly lit with small torches attached to the walls.\n\nAround the outskirts of the main area which you stand are passage ways:\n\nThe passage to the East appears to be closed off by a large block of ice.\n\nThe passage to the North is blocked by raging flames.\n\nThe passage to the West is gaping wide open, however is full of darkness.\n\n");
 
 	for(;;) {
 		printf("What do you do?\n> ");
-	    fgets(input, MAX_INPUT_SZ, stdin);
-	    word = scanner(input, keywords, length);
+		fgets(input, MAX_INPUT_SZ, stdin);
+		word = scanner(input, keywords, length);
 
-	    if(strcmp(word, "east") == 0) {
-	        eastCaverns();
-	    } else if (strcmp(word, "west") == 0) {
+		if(strcmp(word, "east") == 0) {
+			eastCaverns();
+		} else if (strcmp(word, "west") == 0) {
 			westCavernsEntrace(player);
 		} else if (strcmp(word, "north") == 0) {
 			northCaverns();
